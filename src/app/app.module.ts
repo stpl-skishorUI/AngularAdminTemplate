8bf8e1e9be3f0c82901d2ac3e6e14a364e711310
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-06-28 16:51:55 +0530

    angular admin template created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule,  MAT_DATE_LOCALE } from '@angular/material/core';
+import { PartialLayoutComponent } from './partial/partial-layout/partial-layout.component';
+import { FooterComponent } from './partial/partial-layout/footer/footer.component';
+import { HeaderComponent } from './partial/partial-layout/header/header.component';
+import { SidebarComponent } from './partial/partial-layout/sidebar/sidebar.component';
+import { MaterialModule } from './shared/angularMaterialModule/material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartialLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/partial/partial-layout/partial-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partial-layout',
+  templateUrl: './partial-layout.component.html',
+  styleUrls: ['./partial-layout.component.scss']
+})
+export class PartialLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
